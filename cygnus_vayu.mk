2e923a9fff646d15e5135f0b8d4a12a0c237afd3
kuroringo90 <ssss9999@gmx.it>
2022-06-01 23:49:17 +0200

    cygnus_vayu.mk: Adapt inherit

--- cygnus_vayu.mk
+++ cygnus_vayu.mk
@@ -5,9 +5,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit common products
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
