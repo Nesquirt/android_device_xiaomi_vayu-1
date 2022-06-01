169a47c6a3b0e8ab4b060223fed7858912255061
kuroringo90 <ssss9999@gmx.it>
2022-06-01 22:32:00 +0200

    cygnus_vayu.mk: Remove AOSP Inherit

--- cygnus_vayu.mk
+++ cygnus_vayu.mk
@@ -6,8 +6,8 @@
 #
 
 # Inherit common products
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
